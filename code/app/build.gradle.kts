776e96c57b12454a9e31f5f892c274849bc76363
zliang12 <zliang12@ualberta.ca>
2025-09-16 18:22:41 -0600

    small fix 2

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.listycitylab3"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.listycitylab3"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
